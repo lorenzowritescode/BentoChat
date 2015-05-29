b9c3e1d2955ee8db462deb995f9aa400265154db
Sam Budd <budd.samuel@gmail.com>
2015-05-29 16:16:39 +0100

    new prompts

--- front-end/src/components/Chat.js
+++ front-end/src/components/Chat.js
@@ -73,7 +73,7 @@ var ChatList = React.createClass({
     render: function () {
         var MessageListItem = this.state.messages.map(GetMessageList);
         return (
-            <div className="chatlist"   >
+            <div className="chatlist">
                 {MessageListItem}
             </div>
         );
@@ -85,12 +85,18 @@ var ChatList = React.createClass({
 });
 
 var promptArray = [
-    "What you gonna say bitch?",
-    "Spit it out motherfucker",
-    "What is love? What is life?",
-    "Take a hold on your life",
+    "A diamond is merely a lump of coal that did well under pressure.",
+    "Never put off until tomorrow what you can do the day after tomorrow.",
+    "Try to be like the turtle – at ease in your own shell",
+    "Life is like a sewer… what you get out of it depends on what you put into it.",
     "If you try to fail, and succeed, which have you done?",
-    "People Say nothing is impossible, but you do nothing everyday."
+    "People Say nothing is impossible, but you do nothing everyday.",
+    "Even if you are on the right track, you’ll get run over if you just sit there.",
+    "Failure is the condiment that gives success its flavor.",
+    "If you think you are too small to make a difference, try sleeping with a mosquito.",
+    "There are no traffic jams along the extra mile.",
+    "Life is like photography. You need the negatives to develop.",
+    "If you hit the target every time it’s too near or too big."
 ];
 
 function randomPrompt() {
