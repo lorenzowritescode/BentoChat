9247dd84cdab9525f52281868e86d7230d3ad8da
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-06-02 14:44:53 +0100

    Refactored using ES6 class

--- front-end/src/utils/APIUtils.js
+++ front-end/src/utils/APIUtils.js
@@ -3,35 +3,79 @@
  */
 'use strict';
 
-var $ = require('jQuery');
+var request = require('reqwest');
+
+class AjaxRequest {
+    constructor(method) {
+        this._method = method;
+    }
+
+    body (body) {
+        this._body = body;
+        return this;
+    }
+
+    path (path) {
+        this._path = path;
+        return this;
+    }
+
+    callback (callback) {
+        this._callback = callback;
+        return this;
+    }
+
+    context (obj_context) {
+        this._context = obj_context;
+        return this;
+    }
+
+    invokeCallback (resp) {
+        if (this._context !== null && typeof this._context !== 'undefined')
+            this._callback.apply(this._context, resp);
+        else
+            this._callback(resp);
+    }
+
+    run () {
+        var thisReq = this;
+
+        var options = {
+            url: thisReq._path,
+            method: thisReq._method,
+            data: thisReq._body,
+            success: thisReq.invokeCallback.bind(thisReq),
+            error: function (err) {
+                console.err(err.message, err.stack);
+            }
+        };
+
+        request(options);
+    }
+}
 
 module.exports.get = function (path, callback, context) {
-    $.get(path, function (result) {
-        if (context !== null)
-            callback.call(context, result);
-        else
-            callback(result);
-    });
+    new AjaxRequest('get')
+        .path(path)
+        .callback(callback)
+        .context(context)
+        .run();
 };
 
 module.exports.post = function(path, body, callback, context) {
-    $.post(path, body, function(result) {
-        if (context !== null)
-            callback.call(context, result);
-        else
-            callback(result);
-    });
+    new AjaxRequest('post')
+        .path(path)
+        .body(body)
+        .callback(callback)
+        .context(context)
+        .run();
 };
 
 module.exports.put = function(path, body, callback, context) {
-    $.ajax({
-        method: "PUT",
-        url: path,
-        data: body
-    }).done(function (data) {
-        if (callback !== null)
-            callback.apply(context, data);
-        else
-            callback(data);
-    });
+    new AjaxRequest('put')
+        .path(path)
+        .body(body)
+        .callback(callback)
+        .context(context)
+        .run();
 };
