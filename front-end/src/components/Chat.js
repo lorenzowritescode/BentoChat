93cb521f265aa08af2c9048034b51cac80646eaf
Oliver Robinson <evenoli@hotmail.co.uk>
2015-06-05 13:52:39 +0100

    fixes #5

--- front-end/src/components/Chat.js
+++ front-end/src/components/Chat.js
@@ -37,14 +37,33 @@ var ChatAuthor = React.createClass({
     }
 });
 
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+}
+
 //The visual representation of a message
 var ChatMessage = React.createClass({
     render: function () {
         var msg = this.props.message;
+        var linkedMsg = linkify(msg.body);
         return <div className="chat-row">
             <ChatTimestamp timestamp={msg.timestamp} />
             <ChatAuthor name={msg.author} />
-            <div className="chat-text">{msg.body}</div>
+            <div className="chat-text" dangerouslySetInnerHTML={{__html: linkedMsg}}></div>
         </div>;
     }
 });
