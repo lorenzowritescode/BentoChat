841d2f15726deac89c0b02538cd923706f95c8c4
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-06-07 18:49:22 +0100

    indentation and minor changes

--- back-end/scripts/index.js
+++ back-end/scripts/index.js
@@ -1,13 +1,13 @@
 'use strict'  
 const app = require('express')(),
-      server = require('http').Server(app),
-      port = process.env.PORT || 3000,
-      auth = require('./auth.js'),
-      db = require('./db.js'),
-      cors = require('cors'),
-      bodyParser = require('body-parser'),
-      io = require('./socket-config.js')(server),
-      multer = require('multer'); 
+server = require('http').Server(app),
+port = process.env.PORT || 3000,
+auth = require('./auth.js'),
+db = require('./db.js'),
+cors = require('cors'),
+bodyParser = require('body-parser'),
+io = require('./socket-config.js')(server),
+multer = require('multer'); 
 
 app.configure( function () {
     cors();
@@ -17,12 +17,12 @@ app.configure( function () {
       res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Methods", "GET,PUT,POST");
       next();
-    })
+  })
     // This is for decoding json POST requests
     app.use(bodyParser.json());       // to support JSON-encoded bodies
     app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
       extended: true
-    }));
+  }));
     app.use(multer()); // for parsing multipart/form-data
     auth.init(app);
     db.setup();
@@ -32,7 +32,7 @@ app.post('/register', function (req, res) {
     var account = req.body;
 
     auth.createAccount(account, function (err, account) {
-        if(err)
+        if (err)
             res.status(err.status).end(err.userMessage);
         else
             res.send(account);
@@ -41,14 +41,14 @@ app.post('/register', function (req, res) {
 
 app.post('/login', function (req, res) {
     var body = req.body,
-        email = body.email,
-        password = body.password;
+    email = body.email,
+    password = body.password;
 
     auth.login(email, password, function (err, result) {
-      if(err)
-        res.status(err.status).end(err.userMessage);
-
-      res.send(result);
+        if (err)
+            res.status(err.status).end(err.userMessage);
+        else
+            res.send(result);
     })
 })
 
@@ -65,9 +65,9 @@ app.get('/todo', function (req, res) {
   db.getTodos(function(err, result) {
     if (!err)
       res.status(200).send(result).end();
-    else
+  else
       res.status(500).end('Internal Database Error');
-  })
+})
 })
 
 app.post('/todo', function (req, res) {
@@ -77,9 +77,9 @@ app.post('/todo', function (req, res) {
   db.saveTodo (todo, function(err, result) {
     if (!err)
       res.status(200).send(result).end();
-    else
+  else
       res.status(500).end('Internal Database Error');
-  })
+})
 })
 
 app.put('/todo', function(req, res) {
@@ -87,9 +87,9 @@ app.put('/todo', function(req, res) {
   db.toggleTodo (id, function(err, result) {
     if (!err)
       res.status(200).send(result).end();
-    else
+  else
       res.status(500).end('Internal Database Error');
-  })
+})
 })
 
 app.get('/wiki', function (req, res) {
