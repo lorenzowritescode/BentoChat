9dcd4f8b84c61f26a09187f7e5b58b39678e9e56
Oliver Robinson <evenoli@hotmail.co.uk>
2015-06-13 20:47:15 +0100

    Fixed single wiki time stamps, added link functionality to side bar in wiki

--- front-end/src/utils/WikiUtils.js
+++ front-end/src/utils/WikiUtils.js
@@ -9,7 +9,7 @@ var marked = require('marked'),
 
 function Post(title, text) {
     return {
-        author: LoginStore.user.author,
+        author: LoginStore.user.username,
         title: title,
         body: text,
         timestamp: Date.now()
@@ -19,39 +19,18 @@ function Post(title, text) {
 function markdownPost(post) {
     var title = post.title;
     var body = marked(post.body);
+    var date = new Date(parseInt(post.timestamp)).toDateString();
     return (
         <div className="wiki-post">
             <h2>{title}</h2>
-            <sub> written by {post.author} at {post.timestamp}</sub>
+            <sub> written by {post.author} on {date}</sub>
             <p dangerouslySetInnerHTML={{__html: body}}></p>
         </div>
     );
 }
 
-var WikiTimestamp = React.createClass({
-    render: function () {
-        var d = new Date(this.props.timestamp);
-        console.log("Timestamp! "+this.props.timestamp);
-        var mins = d.getMinutes();
-        mins = mins > 9 ? mins : '0' + mins;
-        var hours = d.getHours();
-        hours = hours > 9 ? hours : '0' + hours;
-        var days = d.getDate();
-        days = days > 9 ? days : '0' + days;
-        var month = d.getMonth() + 1; //+1 as Jan is 0
-        month = month > 9 ? month : '0' + month;
-        var year = d.getFullYear();
-        return (
-            <div className="wiki-timestamp">
-                {hours}:{mins} on {days}/{month}/{year}
-            </div>
-        );
-    }
-});
-
 module.exports = {
     Post: Post,
     markdownPost: markdownPost,
-    Comment: Comment,
-    WikiTimestamp: WikiTimestamp
+    Comment: Comment
 };
