2a43c68a4cbe6f6129c1fb655a5376c1e3989934
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-06-13 14:52:09 +0100

    Makes Nav look nicer

--- front-end/src/components/BentoNav.js
+++ front-end/src/components/BentoNav.js
@@ -3,9 +3,58 @@
 var React = require('react/addons');
 var Link = require('react-router').Link;
 var logout = require('../actions/LoginActions').logoutUser;
-
+import GroupStore from '../stores/GroupStore';
+import messageActions from '../actions/messageAction';
 
 require('styles/BentoNav.sass');
+var logo_small = require('../bento-logo-medium.png');
+
+class GroupPicker extends React.Component {
+    constructor () {
+        this.state = {
+            groups: []
+        };
+    }
+
+    componentDidMount () {
+        GroupStore.addChangeListener(this._onChange.bind(this));
+        messageActions.fetchGroups();
+    }
+
+    componentWillUnmount () {
+        GroupStore.removeChangeListener(this._onChange.bind(this));
+    }
+
+    _onChange () {
+        this.setState({
+            groups: GroupStore.groupNames
+        });
+    }
+
+    changeGroup (e) {
+        var selection = e.target.text;
+        if (selection !== GroupStore.current) {
+            messageActions.changeGroup(selection);
+        }
+    }
+
+    render () {
+        var elems = this.state.groups.map((groupName) => {
+            return <li><a onClick={this.changeGroup.bind(this)}>{groupName}</a></li>;
+        });
+
+        return (
+            <div className="navbar-btn group-picker pull-right dropdown">
+                <a href="#" className="btn btn-default dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    Change Group  <span className="caret"></span>
+                </a>
+                <ul className="dropdown-menu" role="menu">
+                    {elems}
+                </ul>
+            </div>
+        );
+    }
+}
 
 var BentoNav = React.createClass({
 
@@ -15,19 +64,21 @@ var BentoNav = React.createClass({
             <div className="container-fluid">
                 <div className="navbar-header">
                     <Link className="navbar-brand" to="app">
-                        BentoChat
+                        <img alt="Brand" src={logo_small} />
                     </Link>
                 </div>
                 <ul className="nav navbar-nav">
                     <li><Link to="chat">Chat</Link></li>
                     <li><Link to="wiki">Wiki</Link></li>
                     <li><Link to="todo">ToDos</Link></li>
+                    <li><Link to="profile">Profile</Link></li>
                 </ul>
                 <button type="button"
                         className="btn btn-info navbar-btn pull-right"
                         onClick={logout}>
                     Logout
                 </button>
+                <GroupPicker />
             </div>
         </div>
       );
