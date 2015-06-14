087ea48dfe10d005fbc2964bc091edeb3e532d77
Sam Budd <budd.samuel@gmail.com>
2015-06-14 17:34:36 +0100

    Tidying up Todos and archive, linkify method refactored into ChatUtils

--- front-end/src/components/Chat.js
+++ front-end/src/components/Chat.js
@@ -10,6 +10,8 @@ var ENTER_KEY_CODE = 13;
 
 require('styles/Chat.sass');
 
+var linkify = require('../utils/ChatUtils').linkify;
+
 //Get the messages from the store
 function getStateFromStores() {
     return {
@@ -38,23 +40,6 @@ var ChatAuthor = React.createClass({
     }
 });
 
-function linkify(inputText) {
-    var replacedText, replacePattern1, replacePattern2, replacePattern3;
-
-    //URLs starting with http://, https://, or ftp://
-    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
-
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
-
-    //Change email addresses to mailto:: links.
-    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
-
-    return replacedText;
-}
 
 //The visual representation of a message
 var ChatMessage = React.createClass({
